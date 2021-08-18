7ef947b0cd4fa053d7b0950c882bff436a140ac1
kumarshantanu01 <shantanuk280@gmail.com>
2021-08-18 21:02:28 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//  { useEffect }
+  
+import axios from 'axios';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state = { advice: " "};
+
+    componentDidMount() {
+      this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get(`https://api.adviceslip.com/advice`)
+        .then((response) => {
+
+            const {advice} = response.data.slip;
+            
+            this.setState({advice});
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render(){
+        // const { advice } = this.state;
+        return(
+            <div className="app">
+            <div className="card">
+              <h1 className="heading">{this.state.advice}</h1>
+              <button className="button" onClick={this.fetchAdvice}>
+                <span>GIVE ME ADVICE!</span>
+              </button>
+            </div>
+          </div>
+    );
+};
 }
 
 export default App;
+
+
+// const App = () => {
+//     useEffect(() => {
+//         async function getData(){
+//             const res = await axios.get(`https://api.adviceslip.com/advice`);
+            
+//             console.log(res.data.slip.advice);
+//         }
+
+//         getData();
+//     }
+//     )
+
+//     return(
+//         <>
+//         <h1>hello</h1>
+//         {/* <h3>{data.advice}</h3> */}
+//         {/* <h3>{res.advice}</h3> */}
+//         {/* <h2>{advice}</h2> */}
+//         {/* <h2>{this.quote.data.slip.advice} </h2> */}
+//         <h2>advice</h2>
+//         {/* {App.res.data.slip.advice} */}
+//         </>
+//     )
+// }
+
